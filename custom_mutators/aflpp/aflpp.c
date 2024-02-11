f5b0127c17890746f454b5002daded82d99aaf13
hxxzhang <zzhhxx000@gmail.com>
2024-02-11 11:26:38 -0800

    test

--- custom_mutators/aflpp/aflpp.c
+++ custom_mutators/aflpp/aflpp.c
@@ -72,7 +72,7 @@ size_t afl_custom_fuzz(my_mutator_t *data, uint8_t *buf, size_t buf_size,
   fuzzer_seed.data_type = TYPE_REQUEST;
   int snd_status;
 
-  if (buf_size*2+1<=2500){
+  if (buf_size*2+1<=2040){
     for (size_t i=0; i< buf_size;i++){
       sprintf(fuzzer_seed.data_buff + (i * 2), "%02X", buf[i]);
     }
