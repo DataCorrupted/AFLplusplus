f6918b33489724d79e535f7b9f979ad2f9be4668
hxxzhang <zzhhxx000@gmail.com>
2024-02-25 23:21:45 -0800

    p

--- custom_mutators/aflpp/aflpp.c
+++ custom_mutators/aflpp/aflpp.c
@@ -120,6 +120,7 @@ size_t afl_custom_fuzz(my_mutator_t *data, uint8_t *buf, size_t buf_size,
     size = size <max_size ? size : max_size; // mutated seed length must less than max size
     // size = buf_size < max_size ? buf_size : max_size;
     memset(data->fuzz_buf, buf, size);
+    printf("not from llm:::");
   }
   *out_buf = data->fuzz_buf;
   return size;
