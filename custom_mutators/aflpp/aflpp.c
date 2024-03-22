fa985f837e528ad3b0d6399515e5ff13bf9e9adf
hxxzhang <zzhhxx000@gmail.com>
2024-03-22 14:25:51 -0700

    text seed test

--- custom_mutators/aflpp/aflpp.c
+++ custom_mutators/aflpp/aflpp.c
@@ -79,15 +79,26 @@ size_t afl_custom_fuzz(my_mutator_t *data, uint8_t *buf, size_t buf_size,
   fuzzer_seed.data_type = TYPE_REQUEST;
   int snd_status;
   // message size is limited to 2048
-  if (buf_size*2+1<=2040){
-    for (size_t i=0; i< buf_size;i++){
-      sprintf(fuzzer_seed.data_buff + (i * 2), "%02X", buf[i]);
-    }
+  
+  //test text info
+  if (buf_size<=2040){
+    memcpy(fuzzer_seed.data_buff,buf,buf_size);
     snd_status = msgsnd(msqid, &fuzzer_seed, buf_size*2+4, 0);
   }
   else{
     snd_status = msgsnd(msqid, &fuzzer_seed, 0, 0);
   }
+  //end test
+
+  // if (buf_size*2+1<=2040){
+  //   for (size_t i=0; i< buf_size;i++){
+  //     sprintf(fuzzer_seed.data_buff + (i * 2), "%02X", buf[i]);
+  //   }
+  //   snd_status = msgsnd(msqid, &fuzzer_seed, buf_size*2+4, 0);
+  // }
+  // else{
+  //   snd_status = msgsnd(msqid, &fuzzer_seed, 0, 0);
+  // }
   memset(fuzzer_seed.data_buff, 0, sizeof(fuzzer_seed.data_buff));
 
   if (snd_status == -1) {
@@ -100,6 +111,7 @@ size_t afl_custom_fuzz(my_mutator_t *data, uint8_t *buf, size_t buf_size,
     printf(":::: my_msg.data_buff %s",my_msg.data_buff);
     // receive non-empty seed(uid+seed)
     if (my_msg.data_type == TYPE_SEED){
+      printf("::::my_msg.data_type == TYPE_SEED");
       size_t hexLength = strlen(my_msg.data_buff);
       // hex string length is even, my_msg.data_buff has redundancies size
       if (hexLength%2!=0) {
